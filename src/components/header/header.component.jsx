9e323b8a6156cb1f846fa034cfd64097cf3331af
Maqsood <SMaqsood@ad.harman.com>
2020-10-20 19:28:34 +0530

    Own Cahnge-Displaying name on Login

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -12,6 +12,9 @@ const Header = ({ currentUser }) => (
       <Logo className="logo" />
     </Link>
     <div className="options">
+      {currentUser != null && currentUser.displayName != null && (
+        <div className="option"> Hi, {currentUser.displayName} </div>
+      )}
       <Link to="/" className="option">
         HOME
       </Link>
@@ -35,8 +38,8 @@ const Header = ({ currentUser }) => (
   </div>
 );
 
-const mapStateToProp = state => ({
-  currentUser: state.user.currentUser
+const mapStateToProp = (state) => ({
+  currentUser: state.user.currentUser,
 });
 
 export default connect(mapStateToProp)(Header);
